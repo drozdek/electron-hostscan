7bebbf9f632c2fcb21c803dbe73958221cb30e99
drozdek <raf.drozdowski@gmail.com>
2022-04-03 17:44:45 +0100

    prettifies the process.ts file

--- src/process.ts
+++ src/process.ts
@@ -1,34 +1,32 @@
-const net = require('net');
-const app = require('electron');
-const dns = require('dns');
-const util = require('util');
+const net = require("net");
+const app = require("electron");
+const dns = require("dns");
+const util = require("util");
 
 $(() => {
-
   /**
    * HostScanner
    *
    * @constructor
    */
-  function HostScanner() { };
+  function HostScanner() {}
 
   // obj ERROR to hold error responses
   let ERROR: object = {},
-    $msg = $('#msg'),
-    $scanResult = $('#scan-result'),
-    $scanResultFailed = $('#scan-result-failed'),
-    $ipAddr = $('#ip_addr');
+    $msg = $("#msg"),
+    $scanResult = $("#scan-result"),
+    $scanResultFailed = $("#scan-result-failed"),
+    $ipAddr = $("#ip_addr");
 
-  $('body').on('click', 'input[type=submit]', (e: any) => {
+  $("body").on("click", "input[type=submit]", (e: any) => {
     $scanResult.empty();
     $scanResultFailed.empty();
 
-    let _val = $('input[type=text]').val();
+    let _val = $("input[type=text]").val();
     if (checkAddr(_val as string)) {
       scanHost();
       return false;
-    }
-    else if (_val === '') {
+    } else if (_val === "") {
       return;
     }
   });
@@ -37,26 +35,27 @@ $(() => {
    * @method getIP - transforms url into its IP equivalent
    *
    * @param {string} url
-   * @returns {string} 
+   * @returns {string}
    */
   let getIP = (url: string): string => {
-    let ip = dns.lookup(url, (error: string, addresses: string, family: string) => {
-      return addresses
-    });
+    let ip = dns.lookup(
+      url,
+      (error: string, addresses: string, family: string) => {
+        return addresses;
+      }
+    );
     return ip;
-  }
+  };
 
   /**
-   * @method checkAddr - checks address/url 
+   * @method checkAddr - checks address/url
    *
    * @param {string} param
    * @returns {boolelan|string}
    */
   let checkAddr = (param: string): boolean | string => {
-    if (typeof param === "undefined" || !getIP(param))
-      return false;
-    else
-      return true;
+    if (typeof param === "undefined" || !getIP(param)) return false;
+    else return true;
   };
 
   /**
@@ -72,34 +71,25 @@ $(() => {
       end = 1000;
 
     while (start <= end) {
-
       ((port) => {
-
         let p = start,
           s = new net.Socket();
 
         s.connect(p, ip, () => {
-          $scanResult.
-            addClass('result_port-open').
-            append(`OPEN: ${p} <br />`);
+          $scanResult.addClass("result_port-open").append(`OPEN: ${p} <br />`);
         });
 
-        s.on('data', (data: object) => {
-          $scanResult.
-            append(`DATA: ${data}`);
+        s.on("data", (data: object) => {
+          $scanResult.append(`DATA: ${data}`);
         });
 
-        s.on('error', (e: string) => {
-
-          if (e === 'ECONNREFUSED') {
+        s.on("error", (e: string) => {
+          if (e === "ECONNREFUSED") {
             return;
           }
-
         });
       })(start);
       start++;
-
     }
   };
-
 });
